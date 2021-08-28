157d6ebc51a5c0c65cf65ad7e45e9a54048f636e
dogaegeol <fbs0219@naver.com>
2021-08-28 16:10:46 +0900

    user_cell()제작

--- 쿼리도.py
+++ 쿼리도.py
@@ -42,7 +42,7 @@ class Object:
         self.size = size
 
 
-board = Object("판.png", [200, 0], (500, 500))
+board = Object("판2.png", [200, 0], (500, 500))
 horizon_wall1 = Object("가로벽.png", [44, 100], (111, 3))
 vertical_wall1 = Object("세로벽.png", [99, 247], (3, 111))
 horizon_wall2 = Object("가로벽.png", [744, 300], (111, 3))
@@ -148,7 +148,46 @@ def click_cell(position):   #ex) location=click_cell(pygame.mouse.get_pos()))
     #print(first_x, mid_x, j)
     return (i,j)
 
+def user_cell(turn):   #ex) location=click_cell(pygame.mouse.get_pos()))
+    if turn=="black":
+        position=black_user.pos
+    else:
+        position=white_user.pos
+    wall_size=0;
     
+    first_x=position[0]-198-56*((position[0]-198)//56)    #0~55
+    i=0
+    if first_x//28>0:
+        mid_x=(first_x-56)*(-1)
+    else:
+        mid_x=first_x
+    if mid_x<=wall_size:
+        if(first_x//28>0):
+            i=(((position[0]-198)//56)+1)*2
+        else:
+            i=((position[0]-198)//56)*2
+    else:
+        i=((position[0]-198)//56)*2+1
+    if (position[0]-198)%56==0:
+        i=((position[0]-198)//56)*2
+    
+    first_y=position[1]+2-56*((position[1]+2)//56)    #0~55
+    j=0
+    if first_y//28>0:
+        mid_y=(first_y-56)*(-1)
+    else:
+        mid_y=first_y
+    if mid_y<=wall_size:
+        if(first_y//28>0):
+            j=(((position[1]+2)//56)+1)*2
+        else:
+            j=((position[1]+2)//56)*2
+    else:
+        j=((position[1]+2)//56)*2+1
+    if (position[1]+2)%56==0:
+        j=((position[1]+2)//56)*2
+    return (i,j)
+
 def user_checker(turn):
     x,y=click_cell(pygame.mouse.get_pos())
     print(board_array[x,y])
